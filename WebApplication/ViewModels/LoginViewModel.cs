53c2a81fd26f7e3992e5efa091c2bb01d22a0915
BeginnerAnatolij <87751340+BeginnerAnatolij@users.noreply.github.com>
2022-04-13 12:14:25 +0300

    Add files via upload

---
+++ WebApplication/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Passowrd")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
